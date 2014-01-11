5d1f6ec7321baba96e3e00e41cb23e4e09a08bab
Allan Palmu <allan.palmu@gmail.com>
2014-01-11 15:52:00 +0200

    first

---
+++ src/main/java/fi/paivola/mapserver/models/TownStorage.java
@@ -0,0 +1,165 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Allan Palmu <allan.palmu@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package fi.paivola.mapserver.models;
+import fi.paivola.mapserver.core.DataFrame;
+import fi.paivola.mapserver.core.Event;
+import java.util.ArrayList;
+import fi.paivola.mapserver.core.ExtensionModel;
+import fi.paivola.mapserver.core.GameManager;
+import fi.paivola.mapserver.core.setting.SettingMaster;
+
+/**
+ * Defines a storage unit for a single town.
+ * @author Allan Palmu <allan.palmu@gmail.com>
+ */
+public class TownStorage extends ExtensionModel {
+    ArrayList<Supplies> storage;
+    double maxCapacity;
+    double currentCapacity;
+    
+    /**
+     * Creates a new storage unit
+     * @param maxCapacity sets the maximum storage capacity for the storage unit
+     */
+    public TownStorage(double maxCapacity, SettingMaster sm){
+        super(5, sm);
+        this.maxCapacity = maxCapacity;
+        storage = new ArrayList<>();
+    }
+    
+    /**
+     * @return the maximum capacity of the storage unit
+     */
+    public double getMax(){
+        return maxCapacity;
+    }
+    
+    /**
+     * @param in the maximum capacity will be set to this
+     */
+    public void setMax(double in){
+        maxCapacity = in;
+    }
+    
+    Supplies findSupplies(int id){
+        for (Supplies s : storage){
+            if (s.id == id)
+                return s;
+        }
+        return null;
+    }
+    
+    /**
+     * Stores the given supplies item into the storage
+     * @param in what to store
+     * @return if the storage filled up, this is how much was left of the item
+     */
+    public double Store(Supplies in){
+        Supplies found = findSupplies(in.id);
+        if (found == null){
+            storage.add(new Supplies(in.id, in.getAmount()+currentCapacity>maxCapacity?maxCapacity-currentCapacity:in.getAmount()));
+        } else {
+            found.setAmount(found.getAmount()+(in.getAmount()+currentCapacity>maxCapacity?maxCapacity-currentCapacity:in.getAmount()));
+        }
+        double overflow = currentCapacity+in.getAmount()-maxCapacity;
+        overflow = overflow<0?0:overflow;
+        Update();
+        return overflow;
+    }
+    
+    /**
+     * @param id the identifier for the type of supplies
+     * @return how much of that we have in stock
+     */
+    public double QuerySupplies(int id){
+        Supplies found = findSupplies(id);
+        if (found == null)
+            return 0;
+        else return found.getAmount();
+    }
+    
+    /**
+     * @return how much stuff is in stock
+     */
+    public double QueryCapacity(){
+        return currentCapacity;
+    }
+    
+    public double QuerySpace(){
+        return maxCapacity - currentCapacity;
+    }
+    
+    /**
+     * Takes a supply item out of the storage
+     * @param id which supplies
+     * @param amount how much
+     * @return the created Supplies item
+     */
+    public Supplies Take(int id, double amount){
+        Supplies found = findSupplies(id);
+        if (found == null){
+            return new Supplies(id, 0);
+        } else {
+            double taken = Math.min(amount, found.getAmount());
+            found.setAmount(found.getAmount() - taken);
+            return new Supplies(id, taken);
+        }
+    }
+    
+    void Update(){
+        currentCapacity = 0;
+        for (Supplies s:storage){
+            if (s.getAmount() == 0){
+                storage.remove(s);
+            }
+            currentCapacity += s.getAmount();
+        }
+        if (currentCapacity > maxCapacity){
+            System.out.println("Storage had more goods than could fit inside. "
+                              +"This should not happen. "
+                              +"Please use the Store method to store items.");
+        }
+    }
+
+    @Override
+    public void onExtensionTick(DataFrame last, DataFrame current) {
+        //rat algorithm
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        //nothing, since events move between towns and not within
+    }
+
+    @Override
+    public void onGenerateDefaults(DataFrame df) {
+        //idk lol
+    }
+
+    @Override
+    public void onRegisteration(GameManager gm, SettingMaster sm) {
+        //idk lol
+    }
+}
