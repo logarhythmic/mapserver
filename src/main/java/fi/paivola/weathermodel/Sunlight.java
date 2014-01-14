6908f9406d120612f8018387efb20f4b8afdf086
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2014-01-14 20:06:58 +0200

    more complete sunlight

--- src/main/java/fi/paivola/weathermodel/Sunlight.java
+++ src/main/java/fi/paivola/weathermodel/Sunlight.java
@@ -4,20 +4,18 @@ package fi.paivola.weathermodel;
  * Sunlight class.
  * @author Jaakko Hannikainen
  */
-public class Rain {
-    public static final double[] rainAvrg = {
-        1, 0, 9, 58,  56, 82, 58, 40,  23, 27, 36, 9};
-    public static final double[] rainProb = {
-        0, 0, 3, 17,  23, 43, 42, 32,  17, 13, 13, 6};
+public class Sunlight {
+    public static final double[] sunAvg = {
+        8.6, 8.9, 9.1, 8.6, 8.8, 7.3, 7.3, 8.2, 8.8, 8.6, 8.7, 8.3 };
 
     // XXX: quick dumb implementation
-    private Rain() {}
+    private Sunlight() {}
 
-    public static double getRain(int week) {
+    public static double getSunlight(int week) {
         if(week < 0 || week > 51)
             throw new IllegalArgumentException("week must be positive");
         week%=52;
         week/=4;
-        return Math.random() * 100 < rainProb[week] ? (rainAvrg[week]:0;
+        return sunAvg[week];
     }
 }
