12b07725e45a0b1060388f91a7d1620015464109
jaakaappi <diapaasi@mail.com>
2014-01-20 21:17:46 +0200

    Commenting, tweaking variables and testing with data

--- src/main/java/fi/water/Lake.java
+++ src/main/java/fi/water/Lake.java
@@ -22,21 +22,32 @@ import java.io.FileWriter;
 
 public class Lake extends PointModel
 {
+    //(Näsijärvi)
+            
     // General stuff
     int height = 0;             // used to check the direction of river runoff
-    CSVWriter writer;
+    CSVWriter writer = null;
     
-    //PET variables
+    // PET variables
     float evapotranspiration;               // mm/day
     float airHumidity;                      // mb
     float temperature = 30;                 // 'C
     float k = 1;                            // Hamon coefficient
     float daytimeLength = 1;                // x/12h
     
-    //Lake dimensions and water amount (Näsijärvi)
+    // Lake dimensions and water amount 
     float surfaceArea = 256.12f;            // km^2
-    float depth = 14.1f;                    // m
+    float depth = 14.1f;                    // m    TODO rename to fit its actual role as the depth of the lake "bowl"
     float waterAmount = 3.48f;              // km^3
+    
+    // Flows
+    float C;                                // Chezy variable
+    
+    // Rain
+    float basinArea = 7642;                 // km^2
+    float rainfall;                         
+    float terrainCoefficient = 0.25f;          // unitless, how much of the rainfall starts to flow above the terrain rather than being absorbed
+    
     
     public Lake(int id)
     {
@@ -60,12 +71,37 @@ public class Lake extends PointModel
             waterAmount = 0;
         }
         else
-        {
+        { 
             waterAmount -= evapotranspiration;
         }
         
-        // write the data to a .csv - file
-        String[] entries = (waterAmount+"#"+evapotranspiration+"#"+temperature).split("#");
+        String[] entries;
+        
+        rainfall = new Random().nextInt(5);
+        float actualRainfall = basinArea*rainfall/1000*terrainCoefficient;
+        waterAmount += actualRainfall;
+        
+        //if the water level is over the boundaries of the lake, count the overflowing amount water
+        if(waterAmount > depth/1000*surfaceArea)
+        {
+            // the overfloawing amount of water
+            float overflow = waterAmount-depth/1000*surfaceArea;
+            // the depth of the overflowing water
+            float currentDepth = overflow/surfaceArea/1000;
+            // calculate the chezy variable used in the chezy flow speed equation
+            C = (float)((1/0.03f)*Math.pow(currentDepth, 1/6));
+            // calculate the speed of the overflowing watermass
+            float speed = (float)(C*Math.sqrt(1*currentDepth));
+            // calculate the amount of water that is going to leave
+            float flow = speed/1000*surfaceArea;
+            // write the data to a .csv - file
+            entries = (waterAmount+"#"+evapotranspiration+"#"+temperature+"#"+actualRainfall+"#"+flow).split("#");
+        }
+        else
+        {
+            // write the data to a .csv - file
+            entries = (waterAmount+"#"+evapotranspiration+"#"+temperature+"#"+actualRainfall).split("#");
+        }
         
         for(int i = 0; i < entries.length; i++)
         {
@@ -93,22 +129,25 @@ public class Lake extends PointModel
     }
 
     @Override
-    public void onGenerateDefaults(DataFrame df) {
+    public void onGenerateDefaults(DataFrame df) 
+    {
+        
     }
     
     @Override
-    public void onUpdateSettings(SettingMaster sm){
+    public void onUpdateSettings(SettingMaster sm)
+    {
         height = Integer.parseInt(sm.settings.get("height").getValue());
         
         if(writer == null)
         {
             try 
             {
-                writer = new CSVWriter(new FileWriter(this.name+".csv"), ',');
+                writer = new CSVWriter(new FileWriter("csv.csv"), ',');
             } 
             catch (IOException e) 
             {
-                System.out.println("Error with the writer");
+                System.out.println(e.toString());
             }
         }
     }
