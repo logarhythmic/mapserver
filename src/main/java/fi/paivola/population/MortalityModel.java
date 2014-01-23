72917aa5d06a61922b1940aed23c7f25203089c4
Henri Lunnikivi <henri.lunnikivi@tut.fi>
2014-01-23 21:15:07 +0200

    Changed one variable name in MortalityModel.java

--- src/main/java/fi/paivola/population/MortalityModel.java
+++ src/main/java/fi/paivola/population/MortalityModel.java
@@ -8,37 +8,37 @@ import fi.paivola.population.Constants;
  * @author Henri Lunnikivi (hegza@paivola.fi)
  */
 public class MortalityModel  {
-    private double[] mortalityRatesPc; // for each age-group per *five* years
+    private double[] baseMortalityRatesPc; // for each age-group per *five* years
     public MortalityModel() {
-        setContentFromDebug();
+        setContentFromtHardcodedData();
     }
 
-    public void setContentFromDebug() {
-        mortalityRatesPc = new double[Constants.NUM_AGE_GROUPS];
-        mortalityRatesPc[Constants.AGES_0TO4] = 0.177355079;
-        mortalityRatesPc[Constants.AGES_5TO9] = 0.023141826;
-        mortalityRatesPc[Constants.AGES_10TO14] = 0.018364581;
-        mortalityRatesPc[Constants.AGES_15TO19] = 0.019372052;
-        mortalityRatesPc[Constants.AGES_20TO24] = 0.025083533;
-        mortalityRatesPc[Constants.AGES_25TO29] = 0.030008579;
-        mortalityRatesPc[Constants.AGES_30TO34] = 0.03533238;
-        mortalityRatesPc[Constants.AGES_35TO39] = 0.042289071;
-        mortalityRatesPc[Constants.AGES_40TO44] = 0.048480605;
-        mortalityRatesPc[Constants.AGES_45TO49] = 0.054892079;
-        mortalityRatesPc[Constants.AGES_50TO54] = 0.067414339;
-        mortalityRatesPc[Constants.AGES_55TO59] = 0.089477594;
-        mortalityRatesPc[Constants.AGES_60TO64] = 0.130027819;
-        mortalityRatesPc[Constants.AGES_65TO69] = 0.196021405;
-        mortalityRatesPc[Constants.AGES_70TO74] = 0.30355144;
-        mortalityRatesPc[Constants.AGES_75TO79] = 0.477421394;
-        mortalityRatesPc[Constants.AGES_80TO84] = 0.708366385;
-        mortalityRatesPc[Constants.AGES_85TO89] = 1.032560043;
-        mortalityRatesPc[Constants.AGES_90TO94] = 1.418019143;
-        mortalityRatesPc[Constants.AGES_95TO99] = 1.940559441;
-        mortalityRatesPc[Constants.AGES_OVER_100] = 1.940559441;
+    public void setContentFromtHardcodedData() {
+        baseMortalityRatesPc = new double[Constants.NUM_AGE_GROUPS];
+        baseMortalityRatesPc[Constants.AGES_0TO4] = 0.177355079;
+        baseMortalityRatesPc[Constants.AGES_5TO9] = 0.023141826;
+        baseMortalityRatesPc[Constants.AGES_10TO14] = 0.018364581;
+        baseMortalityRatesPc[Constants.AGES_15TO19] = 0.019372052;
+        baseMortalityRatesPc[Constants.AGES_20TO24] = 0.025083533;
+        baseMortalityRatesPc[Constants.AGES_25TO29] = 0.030008579;
+        baseMortalityRatesPc[Constants.AGES_30TO34] = 0.03533238;
+        baseMortalityRatesPc[Constants.AGES_35TO39] = 0.042289071;
+        baseMortalityRatesPc[Constants.AGES_40TO44] = 0.048480605;
+        baseMortalityRatesPc[Constants.AGES_45TO49] = 0.054892079;
+        baseMortalityRatesPc[Constants.AGES_50TO54] = 0.067414339;
+        baseMortalityRatesPc[Constants.AGES_55TO59] = 0.089477594;
+        baseMortalityRatesPc[Constants.AGES_60TO64] = 0.130027819;
+        baseMortalityRatesPc[Constants.AGES_65TO69] = 0.196021405;
+        baseMortalityRatesPc[Constants.AGES_70TO74] = 0.30355144;
+        baseMortalityRatesPc[Constants.AGES_75TO79] = 0.477421394;
+        baseMortalityRatesPc[Constants.AGES_80TO84] = 0.708366385;
+        baseMortalityRatesPc[Constants.AGES_85TO89] = 1.032560043;
+        baseMortalityRatesPc[Constants.AGES_90TO94] = 1.418019143;
+        baseMortalityRatesPc[Constants.AGES_95TO99] = 1.940559441;
+        baseMortalityRatesPc[Constants.AGES_OVER_100] = 1.940559441;
     }
     
     public double getAnnualMortalityRate(int ageGroup) {
-        return mortalityRatesPc[ageGroup] * 0.2;
+        return baseMortalityRatesPc[ageGroup] * 0.2;
     }
 }
