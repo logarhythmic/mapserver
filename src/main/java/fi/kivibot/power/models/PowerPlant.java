5c4405b7c1af2102b32b627ae79627954d471cc3
Nicklas Ahlskog <kivi@kivibot.fi>
2014-01-24 10:36:43 +0000

    Solar Power :D

--- src/main/java/fi/kivibot/power/models/PowerPlant.java
+++ src/main/java/fi/kivibot/power/models/PowerPlant.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.kivibot.power.models;
 
 import fi.paivola.mapserver.core.DataFrame;
 import fi.paivola.mapserver.core.Event;
 import fi.paivola.mapserver.core.GameManager;
-import fi.paivola.mapserver.core.Model;
 import fi.paivola.mapserver.core.PointModel;
 import fi.paivola.mapserver.core.setting.SettingMaster;
 import fi.paivola.mapserver.utils.Color;
@@ -20,7 +14,7 @@ import fi.paivola.mapserver.utils.Icon;
  */
 public class PowerPlant extends PointModel {
 
-    private double energy = 10;
+    private double energy = 70;
 
     public PowerPlant(int id) {
         super(id);
@@ -30,6 +24,7 @@ public class PowerPlant extends PointModel {
     @Override
     public void onTick(DataFrame last, DataFrame current) {
         EU eu = new EU(energy);
+        this.saveString("production", eu.toString());
         EU.saveEU(current, this, eu);
     }
 
---
+++ src/main/java/fi/kivibot/power/models/SolarPlant.java
@@ -0,0 +1,27 @@
+package fi.kivibot.power.models;
+
+import fi.paivola.mapserver.core.DataFrame;
+
+/**
+ *
+ * @author kivi
+ */
+public class SolarPlant extends PowerPlant {
+
+    public SolarPlant(int id) {
+        super(id);
+    }
+
+    @Override
+    public void onTick(DataFrame last, DataFrame current) {
+        String sunstr = (String) current.getGlobalData("sunlight");
+        if (sunstr == null) {
+            super.onTick(last, current);
+        } else {
+            EU eu = new EU(Double.parseDouble(sunstr));
+            this.saveString("production", eu.toString());
+            EU.saveEU(current, this, eu);
+        }
+    }
+
+}
